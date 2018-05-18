e94a2534e586587c008853b16562bc21a49ee0fe
Jakub Janarek <jjanarek@gmail.com>
2018-05-18 14:17:40 +0200

    add config from file and recursive parsing to atom

--- lib/barrel/request.ex
+++ lib/barrel/request.ex
@@ -1,9 +1,7 @@
 defmodule BarrelEx.Request do
   use HTTPoison.Base
 
-  @endpoint "http://localhost:7080/"
-
-  # @fields
+  @endpoint Application.get_env(:barrel_ex, :database_url)
 
   def process_url(url) do
     @endpoint <> url
@@ -14,20 +12,41 @@ defmodule BarrelEx.Request do
   end
 
   def process_response_body(body) do
-    Poison.decode!(body)
+    body
+    |> Poison.decode!()
+    # |> atomize()
   end
 
-  # def process_response_body(body) do
-  # implement fields access
-  # end
-  """
-  alias BarrelEx.{
-    Database,
-    Database.Document
-  }
-  with {:ok, db} = Database.get(db) do
-    %{"id": "1234", "name": "Jakub", "surname": "Janarek"}
-    |> Document.create!(db)
+  defp atomize(body) when is_list(body) do
+    for e <- body, do: atomize(e)
   end
-  """
+
+  defp atomize(body) when is_map(body) do
+    Enum.map(body, fn n -> n end)
+  end
+
+  defp atomize(body) when is_binary(body) do
+    String.to_atom(body)
+  end
+
+  defp atomize(body) when is_integer(body) do
+    Integer.to_string(body)
+  end
+
+  defp atomize(body) when is_float(body) do
+    Float.to_string(body)
+  end
+
+  defp atomize(body) when is_atom(body) do
+    body
+  end
+
+  defp atomize(k, v) when is_map(v) do
+    {atomize(k), atomize(v)}
+  end
+
+  defp atomize(k, v) when is_list(v) do
+    {atomize(k), v}
+  end
+
 end
