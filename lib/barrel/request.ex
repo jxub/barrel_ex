8f5ef6d87c1297d6026fc337b871bf935cd1c1db
Jakub Janarek <jjanarek@gmail.com>
2018-05-19 17:59:07 +0200

    add working version of atomize for easier map access

--- lib/barrel/request.ex
+++ lib/barrel/request.ex
@@ -17,39 +17,14 @@ defmodule BarrelEx.Request do
   def process_response_body(body) do
     body
     |> Poison.decode!()
-
-    # |> atomize()
+    |> atomize!()
   end
 
-  defp atomize(body) when is_list(body) do
-    for e <- body, do: atomize(e)
-  end
-
-  defp atomize(body) when is_map(body) do
-    Enum.map(body, fn n -> n end)
-  end
-
-  defp atomize(body) when is_binary(body) do
-    String.to_atom(body)
-  end
-
-  defp atomize(body) when is_integer(body) do
-    Integer.to_string(body)
-  end
-
-  defp atomize(body) when is_float(body) do
-    Float.to_string(body)
-  end
-
-  defp atomize(body) when is_atom(body) do
+  defp atomize!(body) when is_list(body) do
     body
   end
 
-  defp atomize(k, v) when is_map(v) do
-    {atomize(k), atomize(v)}
-  end
-
-  defp atomize(k, v) when is_list(v) do
-    {atomize(k), v}
+  defp atomize!(body) when is_map(body) do
+    Morphix.atomorphiform!(body)
   end
 end
