defda0bd7d83044e28cf4d4d306a9d049d9071ce
Jakub Janarek <jjanarek@gmail.com>
2018-05-19 17:08:32 +0200

    add usage example to main module

--- lib/barrel.ex
+++ lib/barrel.ex
@@ -1,9 +1,21 @@
 defmodule BarrelEx do
   @moduledoc """
   Main module for the code.
+
+  Usage:
+
+  defmodule MyModule do
+  
+    alias BarrelEx.{
+      Database,
+      Document
+    }
+    
+    def myfun do
+      with {:ok, db} = Database.get(db) do
+        Map.new([id: "1234", name: "Jakub", surname: "Janarek"])
+        |> Document.create!(db)
+    end
+  end
   """
-  import BarrelEx.{
-    Database,
-    Document
-  }
 end
