12086130902f0ce6fada74b55718d8b24a6dc7cf
Jakub Janarek <jjanarek@gmail.com>
2018-05-20 12:07:24 +0200

    implement the info and ids functions for server module

--- lib/barrel/server.ex
+++ lib/barrel/server.ex
@@ -5,7 +5,41 @@ defmodule BarrelEx.Server do
   """
   alias BarrelEx.Request
 
+  @doc """
+  Information about Barrel-db version,
+  the description of the Barrel-db application
+  and the URI location of the API doc alongside
+  the request status.
+  """
+  @spec info(none()) :: {atom(), map()}
   def info do
-    nil
+    Request.get()
+  end
+
+  @doc """
+  Information about Barrel-db version,
+  the description of the Barrel-db application
+  and the URI location of the API doc.
+  """
+  @spec info!(none()) :: map
+  def info! do
+    Request.get!()
+  end
+
+  @doc """
+  Returns a new K-Ordered 16-bytes object ID and
+  the request status.
+  """
+  @spec ids(none()) :: {atom(), map()}
+  def ids do
+    Request.get("/ids")
+  end
+
+  @doc """
+  Returns a new K-Ordered 16-bytes object ID.
+  """
+  @spec ids!(none()) :: map
+  def ids! do
+    Request.get!("/ids")
   end
 end
