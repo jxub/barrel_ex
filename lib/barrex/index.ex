1a94739a907f91434e2c9bb2ba9532dd67562024
Jakub Janarek <jjanarek@gmail.com>
2018-06-25 09:52:39 +0200

    update query fn typespec

--- lib/barrex/index.ex
+++ lib/barrex/index.ex
@@ -2,11 +2,11 @@ defmodule Barrex.Index do
   @moduledoc """
   Provides facilities for working with indexes.
   """
-  
+
   @doc """
   Query the barrel indexes.
   """
-  @spec query(String.t(), String.t(), (map(), any()-> atom(), map() | none()) :: any()
+  @spec query(String.t(), String.t(), fun(), any(), map()) :: any()
   def query(barrel, path, fun, acc, opts) do
     :barrel.query(barrel, path, fun, acc, opts)
   end
