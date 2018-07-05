c7cbd62bacc71b293d3aaee9823f677b449fb127
Jakub Janarek <jjanarek@gmail.com>
2018-07-05 11:07:47 +0200

    setup barrel env vars

--- lib/barrex.ex
+++ lib/barrex.ex
@@ -5,8 +5,23 @@ defmodule Barrex do
   
   use Application
 
-  def start(_type, _args) do
-    {:ok, _} = Application.ensure_all_started(:barrel)
+  def start(_app, _type) do
+    :ok = config()
+    {:ok, _} = Application.ensure_all_started(:barrel) 
     {:ok, _} = :barrel_store_sup.start_store(:default, :barrel_memory_storage, %{})
+  end
+
+  def stop(_app) do
+    # :ok = :barrel_store_sup.stop_store(:default)
+    :ok = Application.stop(:barrel)
+  end
+
+  def config do
+    Application.put_env(:barrel, :stores, [])
+    Application.put_env(:barrel, :data_dir, "barrex_data")
+    Application.put_env(:barrel, :ts_file, "barrex_ts")
+    Application.put_env(:barrel_stats, :update_interval, 500)
+    
+    :ok
   end
 end
