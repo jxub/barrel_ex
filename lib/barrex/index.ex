b640639dafd0f5951b4e69ed29c6b8bf8fade9a2
Jakub Janarek <jjanarek@gmail.com>
2018-07-19 00:59:07 +0200

    improve response handling for query

--- lib/barrex/index.ex
+++ lib/barrex/index.ex
@@ -5,9 +5,24 @@ defmodule Barrex.Index do
 
   @doc """
   Query the barrel indexes.
+
+  TODO: try to track down the :barrel.query return values.
   """
-  @spec query(String.t(), String.t(), fun(), any(), map()) :: any()
+  @spec query(String.t(), String.t(), fun, any, map) :: {atom, list}
   def query(barrel, path, fun, acc, opts) do
-    :barrel.query(barrel, path, fun, acc, opts)
+    case :barrel.query(barrel, path, fun, acc, opts) do
+      :ok ->
+        {:ok, nil}
+
+      {:error, reason} ->
+        {:error, reason}
+
+      resp ->
+        {:ok, resp}
+    end
+  end
+
+  def ids(barrel) do
+    query(barrel, "/id", fn doc, acc -> {:ok, [doc["id"] | acc]} end, [], %{})
   end
 end
