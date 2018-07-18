62a852c5fea8c1092599aaf880ec00136c2e4977
Jakub Janarek <jjanarek@gmail.com>
2018-07-19 00:59:28 +0200

    make tests pass again

---
+++ test/barrex_query_test.exs
@@ -0,0 +1,43 @@
+defmodule BarrexQueryTest do
+  use ExUnit.Case
+
+  alias Barrex.{
+    Database,
+    Document,
+    Index
+  }
+
+  setup do
+    Application.ensure_all_started(:barrel)
+    :barrel_store_sup.start_store(:default, :barrel_memory_storage, %{})
+    %{dbname: "testdb"}
+  end
+
+  test "multiple docs", %{dbname: dbname} do
+    Database.delete(dbname)
+    Database.create(dbname)
+
+    with docs <- Stream.repeatedly(&make_person_doc/0) |> Enum.take(40),
+         resps <- Document.save(dbname, docs),
+         ids <-
+           resps
+           |> Enum.map(fn res -> res |> Tuple.to_list() |> Enum.at(1) end) do
+      assert length(ids) == 40
+
+      with {:ok, all} <-
+             Index.query(dbname, "/id", fn doc, acc -> {:ok, [doc["id"] | acc]} end, [], %{}) do
+        assert length(all) == 40
+      end
+    end
+
+    Database.delete(dbname)
+  end
+
+  defp make_person_doc do
+    with age <- :rand.uniform(500),
+         index <- :rand.uniform(3) - 1,
+         name <- Enum.at(["juan", "antonio", "marcos"], index) do
+      %{:name => name, :age => age}
+    end
+  end
+end
--- test/barrex_streams_test.exs
+++ test/barrex_streams_test.exs
@@ -20,23 +20,35 @@ defmodule BarrexStreamsTest do
     :timer.sleep(100)
     stream = Stream.new(dbname)
 
-    {:ok, stream} == Stream.subscribe(stream)
+    {:ok, stream} = Stream.subscribe(stream)
     docs = [%{"id" => "a", "k" => "v"}, %{"id" => "b", "k" => "v2"}]
     [{:ok, "a", rev1}, {:ok, "b", rev2}] = Document.save(dbname, docs)
     :timer.sleep(200)
 
     receive do
       {:changes, stream, changes, last_seq} ->
-        IO.inspect({:changes, stream, changes, last_seq})
+        # IO.inspect({:changes, stream, changes, last_seq})
         # Pattern matching with assert fails on complex maps,
         # so just do a raw `=`.
         seq1 = Enum.at(changes, 0) |> Map.fetch!("seq")
         seq2 = Enum.at(changes, 1) |> Map.fetch!("seq")
 
-        changes = [
-          %{"id" => "a", "rev" => rev1, "seq" => seq1},
-          %{"id" => "b", "rev" => rev2, "seq" => seq2}
-        ]
+        assert changes == [
+                 %{
+                   "id" => "a",
+                   "rev" => rev1,
+                   "seq" => seq1,
+                   "changes" => [rev1],
+                   "doc" => Enum.at(docs, 0)
+                 },
+                 %{
+                   "id" => "b",
+                   "rev" => rev2,
+                   "seq" => seq2,
+                   "changes" => [rev2],
+                   "doc" => Enum.at(docs, 1)
+                 }
+               ]
 
         assert last_seq == seq2
     after
