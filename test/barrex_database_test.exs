dca70abc7033280c47f2849544116a7dafa7625e
Jakub Janarek <jjanarek@gmail.com>
2018-08-08 00:17:15 +0200

    add database tests

---
+++ test/barrex_database_test.exs
@@ -0,0 +1,82 @@
+defmodule BarrexDatabaseTest do
+  use ExUnit.Case
+
+  alias Barrex.{
+    Database
+  }
+
+  setup do
+    Application.ensure_all_started(:barrel)
+    %{db: "testdb", db2: "testdb2"}
+  end
+
+  test "create a new database", dbs do
+    Database.delete(dbs.db)
+    case Database.create(dbs.db) do
+      :ok ->
+        :ok
+
+      other ->
+        raise other
+    end
+  end
+
+  test "create an existing database", dbs do
+    Database.delete(dbs.db)
+    :ok = Database.create(dbs.db)
+    case Database.create(dbs.db) do
+      {:error, :already_exists} ->
+        :ok
+
+      other ->
+        raise other
+    end
+  end
+
+  test "delete a not existing database", dbs do
+    Database.delete(dbs.db)
+    case Database.delete(dbs.db) do
+      {:error, :not_found} ->
+        :ok
+
+      other ->
+        raise other
+    end
+  end
+
+  test "delete an existing database", dbs do
+    Database.create(dbs.db)
+    case Database.delete(dbs.db) do
+      {:ok, :deleted} ->
+        :ok
+
+      other ->
+        raise other
+    end
+  end
+
+  test "get information about an existing database", dbs do
+    Database.create(dbs.db)
+    case Database.info(dbs.db) do
+      {:error, :not_found} ->
+        :ok
+
+      {:ok. info} when is_map(info) ->
+        :ok
+      
+      other ->
+        raise other
+    end
+  end
+
+  test "get information about a not found database", dbs do
+    Database.delete(dbs.db)
+    case Database.info(dbs.db) do
+      {:error, :not_found} ->
+        :ok
+
+      other ->
+        raise other
+    end
+  end
+end
