8625d83a8c092ff67831293f6a008bc555dc7ed1
Jakub Janarek <jjanarek@gmail.com>
2018-08-13 01:14:19 +0200

    fix cursor edge cases, add module documentation and improve cursor tests

--- lib/barrel/cursor.ex
+++ lib/barrel/cursor.ex
@@ -2,26 +2,76 @@ require Record
 
 defmodule Barrel.Cursor do
   @moduledoc """
+  The cursor allows for a series of
+  document transformations, detailed below.
+
+  ### Data transformations and usage
+
+  1. Pass the query options
+
+  The options map allows to pass a `limit`
+  parameter to limit the maximum number of
+  returned documents. The other param,
+  `batch_size` allows to specify how many
+  documents to fetch from the database at the
+  same time. Bigger values of the batch size
+  make usually for an lighter database load.
+
+  2. Fetch by query
+
+  Pass `"id": 1` or `"ids": [1, 2, 3]` to look
+  for documents with a given ID. Otherwise, apply the
+  other query params to all the documents in the database.
+  The database client allows for conditional queries, with
+  the syntax:
+
+  ```
+  $<operator>$<value>
+  ```
+
+  where `<operator>` can be any of "gt, "ge", "lt", "le", or "eq".
+  "eq" is applied by defult if none is present. The `<value>` can
+  be any erlang/elixir term as they all are possible to compare.
+
+  3. Apply the projection
+
+  Use the supplied projection to discard document key-value pairs
+  that are specified with falsey (0 or false) values in the
+  projection map. The keys with thruthy (1||true) values
+  as well as the keys that don't appear in the projection
+  map are shown in the result.
+
+  ### Parameter examples
+
   query:
 
+  ```
   %{
-    "key1": "value1",
-    "k2: 212231
-    # id
+    "key1": "value1", # exact match
+    "k2: "$ge$212231", # look for greater or equal values
+    "id": "23VR94FJ8EJDF330", # look for a specific barrel document id
+    "_rev": "4FQEFPEFJPO" # you can also search for a particular revision id
   }
+  ```
 
   proj:
 
+  ```
   %{
     "id": true,
-    "k2": false,
+    "k2": false
+    # another keys are true or shown by default
   }
+  ```
 
   opts:
 
+  ```
   %{
-    limit: 10
+    :limit => 10,
+    :batch_size => 1 # the default
   }
+  ```
 
   """
 
@@ -45,7 +95,7 @@ defmodule Barrel.Cursor do
       Document
     }
 
-    Record.defrecordp(:state, [:ids, :limit])
+    Record.defrecordp(:state, [:ids, :limit, :batch_size])
 
     def count(_enum) do
       {:error, __MODULE__}
@@ -74,23 +124,26 @@ defmodule Barrel.Cursor do
           raise "inexisting database: #{barrel}"
         end
 
-        limit = opts[:limit]
         # additional opts to be added here and passed to state eventually
-        # batch_size = opts[:batch_size]
+        limit = Map.get(opts, :limit)
+        batch_size = Map.get(opts, :batch_size, 1)
 
         case get_id_or_ids(query) do
           nil ->
             # get all document id's if no id's supplied
             case Document.ids(barrel) do
               {:ok, ids} ->
-                state(ids: ids, limit: limit) |> IO.inspect()
+                state(ids: ids, limit: limit, batch_size: batch_size)
+
+              {:error, :not_found} ->
+                raise "the ids supplied are incorrect"
 
               {:error, reason} ->
                 raise reason
             end
 
-          id_or_ids ->
-            state(ids: id_or_ids, limit: limit) |> IO.inspect()
+          id_or_ids when is_list(id_or_ids) ->
+            state(ids: id_or_ids, limit: limit, batch_size: batch_size)
         end
       end
     end
@@ -98,9 +151,9 @@ defmodule Barrel.Cursor do
     @spec get_id_or_ids(map()) :: list()
     defp get_id_or_ids(query) do
       # prioritize :id in query to use also to find one doc
-      case query[:id] do
+      case query["id"] do
         nil ->
-          case query[:ids] do
+          case query["ids"] do
             nil ->
               nil
 
@@ -125,20 +178,33 @@ defmodule Barrel.Cursor do
         state(ids: []) = state ->
           {:halt, state}
 
-        state(ids: ids, limit: limit) = state ->
-          with {id, new_ids} <- ids |> List.pop_at(0) |> IO.inspect() do
-            case Document.fetch_one(barrel, id) do
-              {:ok, doc} ->
-                case satisfies_query(doc, query) do
-                  true ->
-                    with projected_doc <- satisfies_proj(doc, proj) do
-                      {[projected_doc], state(state, ids: new_ids, limit: update_limit(limit))}
-                    end
+        state(batch_size: 0) = state ->
+          {:halt, state}
 
-                  false ->
-                    with empty_doc <- Map.new() do
-                      {[empty_doc], state(state, ids: new_ids, limit: update_limit(limit))}
-                    end
+        state(ids: ids, limit: limit, batch_size: batch_size) = state ->
+          with ids_batch <- ids |> Enum.take(batch_size),
+               pending_ids <- ids |> Enum.drop(batch_size) do
+            case Document.fetch(barrel, ids_batch) do
+              {:ok, resps} ->
+                with filtered <-
+                       Enum.map(resps, fn
+                         {:ok, doc} ->
+                           doc_satisfies_query(doc, query)
+
+                         {:error, _why} ->
+                           %{}
+                       end)
+                       |> Enum.filter(fn doc -> doc != %{} end),
+                     projected <- Enum.map(filtered, fn doc -> satisfies_proj(doc, proj) end) do
+                  {
+                    projected,
+                    state(
+                      state,
+                      ids: pending_ids,
+                      limit: update_limit(limit, batch_size),
+                      batch_size: batch_size
+                    )
+                  }
                 end
 
               {:error, :not_found} ->
@@ -155,6 +221,11 @@ defmodule Barrel.Cursor do
     Removes all the fields from `doc` which are represented by a
     falsey (0 or false) value in projection.
     """
+    @spec satisfies_proj(map, map) :: map
+    defp satisfies_proj(doc, proj) when doc == %{}, do: %{}
+    defp satisfies_proj(doc, proj) when proj == %{}, do: doc
+
+    @spec satisfies_proj(map, map) :: map
     defp satisfies_proj(doc, proj) do
       with falsey <- Enum.reject(proj, fn {k, v} -> is_truthy(k, v) end),
            not_projected <- falsey |> Enum.into(%{}) |> Map.keys() do
@@ -167,11 +238,13 @@ defmodule Barrel.Cursor do
     If there is no limit set, do not decrement it,
     effectively creating a potentially very large loop over
     all the id's in the database in the worst case.
+
+    The default batch size is set to 1, same as in `start_fun/1`.
     """
-    defp update_limit(limit) do
+    defp update_limit(limit, batch_size \\ 1) do
       case limit do
         nil -> nil
-        l when is_integer(l) -> l - 1
+        l when is_integer(l) -> l - batch_size
       end
     end
 
@@ -181,32 +254,92 @@ defmodule Barrel.Cursor do
     """
     defp after_fun(_) do
       fn _ ->
-        # IO.inspect(res)
         :ok
       end
     end
 
-    @spec satisfies_query(map, none()) :: boolean()
-    defp satisfies_query(doc, nil), do: true
+    # @spec doc_satisfies_query(map, map | none) :: map
+    # defp doc_satisfies_query(doc, query) when is_nil(query), do: doc
+    # defp doc_satisfies_query(doc, query) when query == %{}, do: doc
 
-    @spec satisfies_query(map, map) :: boolean()
-    defp satisfies_query(doc, query) when query == %{}, do: true
-
-    @spec satisfies_query(map, map) :: boolean()
-    defp satisfies_query(doc, query) do
-      with query_keys <- Map.keys(query),
-           doc_affected <- Map.take(doc, query_keys),
-           doc_keys <- Map.keys(doc_affected) do
-        for dk <- doc_keys do
-          if doc_affected[dk] != query[dk] do
-            # TODO: add more rule-matching queries with $gt and $lt,...
-            # at the moment, only equality is supported
-            false
-          end
+    @spec doc_satisfies_query(map, map) :: map
+    defp doc_satisfies_query(doc, query) do
+      # remember that the id keys are useful only at fetch time, maybe do that in start_fun
+      with id_keys <- ["id", "ids"],
+           query <- Map.drop(query, id_keys) do
+        if query
+           |> Map.keys()
+           |> Enum.any?(fn key ->
+             not passes_value_filter?(Map.fetch!(doc, key), Map.fetch!(query, key))
+           end) do
+          %{}
+        else
+          doc
         end
       end
+    end
 
-      true
+    @doc """
+    Look for presence of special characters in the passed
+    query value that allow for other comparisions than equality.
+
+    The avaliable comparisions for now are:
+
+    + "$gt$" or > that allows only bigger values in the result set, as in
+    the example query: %{:age => "$gt$18"} to show only documents where age = 18
+
+    + "$ge$" or >=
+
+    + "$lt$" or <
+
+    + "$le$" or <=
+
+    + "$eq$" or == which is also the **default** in case no special value filter is present
+
+    All the previous comparisions are valid for all elrang temrs, integers and strings are among them.
+    That means that you can even filter by alphabetical order in search.
+    """
+    @spec passes_value_filter?(any, any) :: boolean()
+    defp passes_value_filter?(doc_val, query_val) do
+      case get_compare_op(query_val) do
+        {nil, query_val} ->
+          Kernel.==(doc_val, query_val)
+
+        {"gt", updt_query_val} ->
+          Kernel.>(doc_val, updt_query_val)
+
+        {"ge", updt_query_val} ->
+          Kernel.>=(doc_val, updt_query_val)
+
+        {"lt", updt_query_val} ->
+          Kernel.<(doc_val, updt_query_val)
+
+        {"le", updt_query_val} ->
+          Kernel.<=(doc_val, updt_query_val)
+
+        {"eq", updt_query_val} ->
+          Kernel.==(doc_val, updt_query_val)
+
+        {other_op, _query_val} ->
+          raise "invalid query filter operation: #{other_op}"
+      end
+    end
+
+    @doc """
+    Check for presence and extract the comparision
+    operator from the string query value.
+    """
+    defp get_compare_op(query_val) do
+      case query_val |> to_string() |> String.starts_with?("$") do
+        true ->
+          with tokens <- query_val |> String.split("$"),
+               ["", compare_op, new_query_val] <- tokens do
+            {compare_op, new_query_val}
+          end
+
+        false ->
+          {nil, query_val}
+      end
     end
 
     @spec is_truthy(any, any) :: boolean()
--- test/barrel_cursor_test.exs
+++ test/barrel_cursor_test.exs
@@ -28,7 +28,7 @@ defmodule BarrelCursorTest do
     {:ok, doc_id, rev_id} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
     {:ok, doc_id1, rev_id1} = Document.save_one(dbs.db, %{:c => 2, :d => 4})
 
-    with query <- %{:ids => [doc_id, doc_id1]},
+    with query <- %{"ids" => [doc_id, doc_id1]},
          doc_keys <- [:a, :b, "_rev", "id"],
          c <- %Cursor{barrel: dbs.db, query: query, proj: %{}, opts: %{}},
          results <- c |> Enum.to_list() do
@@ -45,8 +45,8 @@ defmodule BarrelCursorTest do
     Database.delete(dbs.db)
     Database.create(dbs.db)
 
-    {:ok, doc_id, rev_id} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
-    {:ok, doc_id1, rev_id1} = Document.save_one(dbs.db, %{:a => 2, :b => 4})
+    {:ok, _doc_id, _rev_id} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
+    {:ok, _doc_id1, _rev_id1} = Document.save_one(dbs.db, %{:a => 2, :b => 4})
 
     with c <- %Barrel.Cursor{
            barrel: dbs.db,
@@ -65,17 +65,69 @@ defmodule BarrelCursorTest do
     end
   end
 
-  test "check if limit worls properly", dbs do
+  test "check if limit works properly", dbs do
     Database.delete(dbs.db)
     Database.create(dbs.db)
 
-    {:ok, doc_id, rev_id} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
-    {:ok, doc_id1, rev_id1} = Document.save_one(dbs.db, %{:a => 2, :b => 4})
-    {:ok, doc_id2, rev_id2} = Document.save_one(dbs.db, %{:a => 4, :b => 6})
+    {:ok, _doc_id, _rev_id} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
+    {:ok, _doc_id1, _rev_id1} = Document.save_one(dbs.db, %{:a => 2, :b => 4})
+    {:ok, _doc_id2, _rev_id2} = Document.save_one(dbs.db, %{:a => 4, :b => 6})
 
     with c <- %Barrel.Cursor{barrel: dbs.db, query: %{}, proj: %{}, opts: %{:limit => 2}},
          results <- c |> Enum.to_list() do
       assert length(results) == 2
+    end
+  end
+
+  test "check if batch size returns n documents", dbs do
+    Database.delete(dbs.db)
+    Database.create(dbs.db)
+
+    {:ok, _doc_id0, _rev_id0} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
+    {:ok, _doc_id1, _rev_id1} = Document.save_one(dbs.db, %{:a => 1, :b => 4})
+    {:ok, _doc_id2, _rev_id2} = Document.save_one(dbs.db, %{:a => 1, :b => 6})
+    {:ok, _doc_id3, _rev_id3} = Document.save_one(dbs.db, %{:a => 1, :b => 2})
+    {:ok, _doc_id4, _rev_id4} = Document.save_one(dbs.db, %{:a => 1, :b => 4})
+    {:ok, _doc_id5, _rev_id5} = Document.save_one(dbs.db, %{:a => 1, :b => 6})
+
+    with c <- %Barrel.Cursor{
+           barrel: dbs.db,
+           query: %{:a => 1},
+           proj: %{:a => 0, :b => true},
+           opts: %{:batch_size => 2}
+         },
+         results <- c |> Enum.to_list() do
+      assert length(results) == 6
+    end
+  end
+
+  test "check if conditional querying returns the desired documents", dbs do
+    Database.delete(dbs.db)
+    Database.create(dbs.db)
+
+    {:ok, _doc_id0, _rev_id0} = Document.save_one(dbs.db, %{:age => "20", :name => "Diego"})
+    {:ok, _doc_id1, _rev_id1} = Document.save_one(dbs.db, %{:age => "20", :name => "Jorge"})
+    {:ok, _doc_id2, _rev_id2} = Document.save_one(dbs.db, %{:age => "18", :name => "Juan"})
+    {:ok, _doc_id3, _rev_id3} = Document.save_one(dbs.db, %{:age => "21", :name => "Santi"})
+    {:ok, _doc_id4, _rev_id4} = Document.save_one(dbs.db, %{:age => "20", :name => "Jakub"})
+    {:ok, _doc_id5, _rev_id5} = Document.save_one(dbs.db, %{:age => "1", :name => "Toni"})
+
+    with c <- %Barrel.Cursor{
+           barrel: dbs.db,
+           query: %{:age => "$gt$18"},
+           proj: %{"_rev" => false, "id" => false, :age => true, :name => true},
+           opts: %{}
+         } do
+      results = c |> Enum.to_list()
+
+      assert length(results) == 4
+
+      for result <- results do
+        for key <- Map.keys(result) do
+          assert key not in ["_rev", "id"]
+          assert key in [:name, :age]
+        end
+      end
     end
   end
 end
